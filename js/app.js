a75dc02f26e8fac68fc1b68cbf3509e7f11e3041
manoj-shetty <manojs231@outlook.com>
2023-09-08 19:06:01 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('mousemove', e => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
+		`
+	})
+})
+
+document.getElementById("btn").addEventListener("click",() => {
+	window.location.href = "../portfolio";
+});
