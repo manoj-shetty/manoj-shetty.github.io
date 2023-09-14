d6f35142c4454e88067cc0a73914bb3c5d2a932e
manoj-shetty <manojs231@outlook.com>
2023-09-14 19:51:07 +0530

    added popup/toast

--- js/app.js
+++ js/app.js
@@ -7,6 +7,37 @@ document.addEventListener('mousemove', e => {
 	})
 })
 
-document.getElementById("btn").addEventListener("click",() => {
+document.getElementById("btn").addEventListener("click", () => {
 	window.location.href = "../portfolio";
+});
+
+
+// for popup
+toast = document.querySelector(".toast")
+closeIcon = document.querySelector(".close"),
+	progress = document.querySelector(".progress");
+
+let timer1, timer2;
+document.addEventListener('DOMContentLoaded', function () {
+	toast.classList.add("active");
+	progress.classList.add("active");
+
+	timer1 = setTimeout(() => {
+		toast.classList.remove("active");
+	}, 5000); //1s = 1000 milliseconds
+
+	timer2 = setTimeout(() => {
+		progress.classList.remove("active");
+	}, 5300);
+});
+
+closeIcon.addEventListener("click", () => {
+	toast.classList.remove("active");
+
+	setTimeout(() => {
+		progress.classList.remove("active");
+	}, 300);
+
+	clearTimeout(timer1);
+	clearTimeout(timer2);
 });
